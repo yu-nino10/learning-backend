de0fa365f2a9d085fe701c31ec84d1d13108fa9c
yu-nino10 <yunino.0922.rg@gmail.com>
2026-01-29 22:15:22 +0900

    課題３作成

---
+++ src/Dtos/HttpUserRequestDto.cs
@@ -0,0 +1,7 @@
+namespace Dtos;
+
+public class HttpUserRequestDto
+{
+    public string? Name {get; set;}
+    public string? Email {get; set;}
+}
--- src/Endpoints/HttpEndpoint.cs
+++ src/Endpoints/HttpEndpoint.cs
@@ -1,14 +1,20 @@
+using System.Reflection.Metadata;
+using Dtos;
+
+namespace Endpoints;
+
 public static class HttpEndpoint
 {
     public static void RegisterHttpEndpoint(this WebApplication app)
     {
         app.MapGet("/request-info", getRequestInfo);
         app.MapGet("/search", getSearchInfo);
+        app.MapGet("/api/users", PostUsers);
         
     }
 
     /// <summary>
-    /// 課題1　リクエスト情報を取得して返す
+    /// 課題１　リクエスト情報を取得して返す
     /// </summary>
     /// <param name="context"></param>
     /// <returns>リクエスト情報</returns>
@@ -28,7 +34,7 @@ public static class HttpEndpoint
     }
 
     /// <summary>
-    /// 課題2　クエリパラメータを取得する
+    /// 課題２　クエリパラメータを取得する
     /// </summary>
     /// <param name="keyword"></param>
     /// <param name="limit"></param>
@@ -45,4 +51,24 @@ public static class HttpEndpoint
         return TypedResults.Ok(result);
     }
 
+    /// <summary>
+    /// 課題３ リクエストボディとレスポンスステータス
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    public static IResult PostUsers(HttpUserRequestDto dto)
+    {
+        if (string.IsNullOrEmpty(dto.Name) || string.IsNullOrEmpty(dto.Email) )
+        {
+            return TypedResults.BadRequest(new {error = "不正な値です。"});
+        }
+
+        if(dto.Email.Contains("@")) {
+            return TypedResults.BadRequest(new {error = "Emailに@が含まれていません。"});
+        }
+        
+        var result = new { message = "User created", name = dto.Name};
+
+        return TypedResults.Created("/api/users", result); // TODO Createdを使う？
+    }
 }
